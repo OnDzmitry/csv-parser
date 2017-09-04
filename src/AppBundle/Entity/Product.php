b89b0aea36f2e83e14fa420ccfb4c7889b688e17
Ozxx <dimonkozlovskiy@gmail.com>
2017-09-04 15:26:57 +0300

    added migration and some fields for Product

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -22,9 +22,31 @@ class Product
     private $id;
 
     /**
+     * @var float
+     *
+     * @ORM\Column(name="dbProductCost", type="float")
+     *
+     * @Assert\GreaterThan(1000)
+     * @Assert\Type("float", message="The value {{ value }} is not a valid {{ type }}.")
+     */
+    private $cost;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="intProductStock", type="integer")
+     *
+     * @Assert\LessThan(10)
+     */
+    private $stock;
+    /**
      * @var string
      *
      * @ORM\Column(name="strProductName", type="string", length=50, nullable=false)
+     *
+     *
+     * @Assert\NotNull(message="Product name must be requare")
+     * @Assert\Type("string", message="The value {{ value }} is not a valid {{ type }}.")
      */
     private $name;
 
@@ -62,6 +84,150 @@ class Product
      * @ORM\Column(name="stmTimestamp", type="datetime", nullable=false)
      */
     private $timestamp = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * @param float $cost
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * @param int $stock
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+
+    /**
+     * @param string $desc
+     */
+    public function setDesc($desc)
+    {
+        $this->desc = $desc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getAddAt()
+    {
+        return $this->addAt;
+    }
+
+    /**
+     * @param \DateTime $addAt
+     */
+    public function setAddAt($addAt)
+    {
+        $this->addAt = $addAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDiscontinued()
+    {
+        return $this->discontinued;
+    }
+
+    /**
+     * @param \DateTime $discontinued
+     */
+    public function setDiscontinued($discontinued)
+    {
+        $this->discontinued = $discontinued;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param \DateTime $timestamp
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
 
 }
 
